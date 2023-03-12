a080e376b668109904d7c46edd5b3717f9bea3fe
oberdan <oberdan.dev@gmail.com>
2023-03-12 20:54:09 -0300

    add some styles to nav

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
+import twitterLogo from './assets/twitter-logo.svg'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className='layout'>
       <aside className="sidebar">
-        <img src="" alt="logo" />
-        <nav>
+        <img src={twitterLogo} className="logo" alt="logo" />
+        <nav className="main-navigation">
           <a>home</a>
         </nav>
-        <button>Tweet</button>
+        <button className="new-tweet" type="button">Tweet</button>
       </aside>
       <div className="content">
         content
